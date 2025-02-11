edbefc279f0172c174678984e823a66407ce00f6
Leleu Vilela <viniciusvilela19@gmail.com>
2025-02-11 11:50:21 +0000

    Update mockoon

--- pkgs/by-name/mo/mockoon/package.nix
+++ pkgs/by-name/mo/mockoon/package.nix
@@ -6,11 +6,11 @@
 
 let
   pname = "mockoon";
-  version = "6.1.0";
+  version = "9.1.0";
 
   src = fetchurl {
     url = "https://github.com/mockoon/mockoon/releases/download/v${version}/mockoon-${version}.AppImage";
-    hash = "sha256-harZU3TTIzfJoY/jAQI0dm7YSOr24Y9xk9L5ZaBLdD8=";
+    hash = "...";
   };
 
   appimageContents = appimageTools.extractType2 {
